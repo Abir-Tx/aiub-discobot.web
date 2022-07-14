9fd1f8eb74319dc67db179306960fb3883523944
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-07-15 00:39:36 +0600

    Add comment for the github_url openning func

--- res/js/custom/shortcuts.js
+++ res/js/custom/shortcuts.js
@@ -1,7 +1,7 @@
 // bind !gh to open github source code repo. This will work as an easter egg on the site
 Mousetrap.bind('! g h', open_github_source);
 
-
+// Function to open the github source code of the repository in a new tab or in the same tab. 
 function open_github_source() {
 	console.log('Opening github source code repo');
 	// Open in current window
