d428f44dfb06ee7c2df7c30fa6c712172df3c9bc
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2022-07-15 16:26:47 +0600

    Added a footer & hidden keyboard shortcuts on site (#12)
    * Add `SASS` version
    
    Added SASS version details as SASS is being used from now on for custom CSS
    
    * Create custom `SCSS`
    
    * Add  `.vscode` to gitignore
    
    - ignored my custom vscode settings for this repo as it may confuse new contributors
    
    * Add hover style for the footer link
    
    * Move the `.landing` custom css codes to scss file 📁🗄
    
    * Fix footer hover effect
    
    - Added a new footer section
    - Removed bs5 color on footer
    - Added custom color to the footer
    
    * Add mousestrap js cdn and my cutom js link
    
    Mousestrap JS is used to control keyboard shortcuts on a website using js
    
    * Add `!gh` keybind for openning github repo of this project
    
    * Create notes file for using as dev ref
    
    * Add features section
    
    - Added keyboard shotcuts descriptions
    
    * Add reference site
    
    * Create `help` menu for the website.
    
    - This menu will also be hidden & will only be triggered if `?` is pressed.
    - The menu will be the ultimate place for discovering the other available shortcuts of the website
    
    * Create the shortcut for `?` key
    
    - This key will trigger the #help modal of the page
    
    * Add `!v` keyboard binding
    
    This will show the version informations
    
    * Add `!gi` shortcut for issue submission
    
    * Add title change when `!gh` is issued
    
    * Add github logo with repo link to the footer

---
+++ docs/Developement.md
@@ -0,0 +1,17 @@
+# Developement Notes
+
+Use this file for reference for developement setup for future. Notes about used frameworks, methods, special notes etc will be writen down here so that new developers does not get confused or lost. 
+
+
+## Mousestrap JS
+
+Added **Mousestrap JS** library to the project to add keyboard shortcuts responds to the website. Key bindings are coded in the `shortcuts.js` file. 
+
+- Website of Mousestarp JS -> https://craig.is/killing/mice
+- CDNJS page -> https://cdnjs.com/libraries/mousetrap
+- Reference Site -> https://www.hongkiat.com/blog/keyboard-shortcuts-website/
+
+
+## Icons Used
+
+Google Icons are used -> https://fonts.google.com/icons?icon.query=help
---
+++ res/js/custom/shortcuts.js
@@ -0,0 +1,43 @@
+// bind !gh to open github source code repo. This will work as an easter egg on the site
+Mousetrap.bind('! g h', open_github_source);
+
+// Function to open the github source code of the repository in a new tab or in the same tab. 
+function open_github_source() {
+	console.log('Opening github source code repo');
+	// Open in current window
+	window.location.href = "http://www.github.com/abir-tx/aiub-discobot.web";
+	// Open in a new window
+	// window.open("http://www.github.com/abir-tx/aiub-discobot.web");
+
+
+	// Change the title of the tab when the !gh shortcut is issued (stated in #15)
+	document.title = "Openning GitHub Repo...";
+}
+
+
+
+// Bind ? key for openning the help modal. 
+Mousetrap.bind('?', function () {
+	var helpModal = new bootstrap.Modal(document.getElementById('help'), {
+		keyboard: true, /* If true Closes the modal when escape key is pressed. */
+		backdrop: true
+	});
+	helpModal.show();
+})
+
+
+// Bind !v for openning the website version and author information.
+Mousetrap.bind('! v', version_info)
+
+function version_info() {
+	var versionModal = new bootstrap.Modal(document.getElementById('version'), {
+		keyboard: true, /* If true Closes the modal when escape key is pressed. */
+		backdrop: true
+	});
+	versionModal.show();
+}
+
+// Bind !ghi for reporting issues to github issue
+Mousetrap.bind('! g i', function () {
+	window.location.href = "https://github.com/Abir-Tx/aiub-discobot.web/issues/new/choose";
+})
