58647dfea20528ed7d5bcc042b023671882e5203
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-07-18 00:45:19 +0600

    Create a globa js file
    - This file will hold important global variables
    - This will help to change or update the values easily
    - Helps in management

---
+++ res/js/custom/global.js
@@ -0,0 +1,10 @@
+// This is a file for changing global variables used widely in html or other JS. This file
+// is intended to be the one stop place to change or update important files or text i.e 
+// version number
+// By: Mushfiqur Rahman Abir
+// Creation Date: 18 July 2022
+
+
+// current version variable
+var current_web_version = 'v1.5.0';
+
