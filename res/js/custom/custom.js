bd9d2c0c202de90444c786214de2ada4ac74dfc7
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-07-18 00:54:43 +0600

    Make the version showing easier
    - Fetches the `version` information from the `global.js` file on runtime. So It will be easy to update the version info
    - Removed the todo comment from the `index.html` file
    - Added needed `id` for the two version showing tags
    - Created a function to update the website version from the `global.js` file
    
    --------------
    Signed Off By: @Abir-Tx

--- res/js/custom/custom.js
+++ res/js/custom/custom.js
@@ -6,7 +6,6 @@
 var by_abir = document.getElementById("by_abir");
 function myFunction(x) {
 	if (x.matches) { // If media query matches
-		console.log(by_abir.innerHTML);
 		by_abir.innerHTML = "By Mushfiqur Rahman Abir";
 	} else {
 		by_abir.innerHTML = "By Abir";
@@ -16,3 +15,14 @@ function myFunction(x) {
 var x = window.matchMedia("(min-width: 840px)")
 myFunction(x) // Call listener function at run time
 x.addListener(myFunction) // Attach listener function on state changes
+
+
+// Function to change the version informations on places of HTML 
+function updateWebVersion() {
+	var version_modal = document.getElementById('version_modal');
+	var version_footer = document.getElementById('version_footer');
+	version_modal.innerHTML = current_web_version;
+	version_footer.innerHTML = current_web_version;
+}
+
+updateWebVersion();
