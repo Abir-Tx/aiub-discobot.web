8fb85699d7d6dfdae5646872b5f58c0f9cedf5db
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2022-09-16 17:33:23 +0600

    [BUG] The version info shows version for the Discord Bot currently not the website
    - `global.js` is now no longer needed for the version loading
    - Used `octokit.js` for loading version info using github API
    - Transformed the scripts loading type to module in order to load the octokit js from CDN
    - Reformated some of the codebase
    
    closes #23
    
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>

--- res/js/custom/custom.js
+++ res/js/custom/custom.js
@@ -1,28 +1,36 @@
 /* The cusotom javascript file for implementing custom conditions and codes dynamically */
 
-
-
 /* Change the footer (By .....) text accordingly with respect to the device size */
+import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
 var by_abir = document.getElementById("by_abir");
 function myFunction(x) {
-	if (x.matches) { // If media query matches
-		by_abir.innerHTML = "By Mushfiqur Rahman Abir";
-	} else {
-		by_abir.innerHTML = "By Abir";
-	}
+  if (x.matches) {
+    // If media query matches
+    by_abir.innerHTML = "By Mushfiqur Rahman Abir";
+  } else {
+    by_abir.innerHTML = "By Abir";
+  }
 }
 
-var x = window.matchMedia("(min-width: 840px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+var x = window.matchMedia("(min-width: 840px)");
+myFunction(x); // Call listener function at run time
+x.addListener(myFunction); // Attach listener function on state changes
 
+// Function to change the version informations on places of HTML
+async function updateWebVersion() {
+  const octokit = new Octokit();
+  var current_web_version = "v1.0.0";
 
-// Function to change the version informations on places of HTML 
-function updateWebVersion() {
-	var version_modal = document.getElementById('version_modal');
-	var version_footer = document.getElementById('version_footer');
-	version_modal.innerHTML = current_web_version;
-	version_footer.innerHTML = current_web_version;
-};
+  await octokit
+    .request("GET /repos/{owner}/{repo}/releases/latest", {
+      owner: "Abir-Tx",
+      repo: "aiub-discobot.web",
+    })
+    .then((res) => (current_web_version = res["data"].tag_name));
+  var version_modal = document.getElementById("version_modal");
+  var version_footer = document.getElementById("version_footer");
+  version_modal.innerHTML = current_web_version;
+  version_footer.innerHTML = current_web_version;
+}
 
-// TODO: Get this version info dynamically using the github API. Ref: https://github.com/octokit/octokit.js#usage
+export { updateWebVersion };
--- res/js/custom/global.js
+++ res/js/custom/global.js
@@ -1,10 +1,13 @@
 // This is a file for changing global variables used widely in html or other JS. This file
-// is intended to be the one stop place to change or update important files or text i.e 
+// is intended to be the one stop place to change or update important files or text i.e
 // version number
 // By: Mushfiqur Rahman Abir
 // Creation Date: 18 July 2022
 
-
 // current version variable
-var current_web_version = 'v1.5.0';
+/* The current_web_version variable here is not longer needed
+ as of 9/16/22 the current_web_version var now loads the value
+  dynamically from the github api in the custom.js file 
+*/
 
+// var current_web_version = "v1.5.0";
